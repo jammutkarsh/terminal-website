1f7c52c43a0052b0dd355df532d2704f1205871f
JammUtkarsh <5.utkarshchourasia@gmail.com>
2022-06-06 10:30:23 +0530

    Head added & changes in color scheme and favicon

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Terminal from "../components/Terminal";
 
 import styles from "../styles/Home.module.css";
 
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Utkarsh Chourasia</title>
+        <link rel="My Icon" href="/favicon.png" />
+      </Head>
       <h1>
         Utkarsh:$ <span className={styles.help}>type help to start</span>
       </h1>
